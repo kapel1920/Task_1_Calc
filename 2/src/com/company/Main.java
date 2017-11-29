07ce4461f8bfb1f9173b627b12a8ace19a142728
Andrey Veselkov <kapel1920@rambler.ru>
2017-11-29 17:27:12 +0300

    Task_1 first commit без округления

---
+++ 2/src/com/company/Main.java
@@ -0,0 +1,95 @@
+package com.company;
+
+import java.math.RoundingMode;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main
+{
+    //Ввод данных
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        //int a = getInt();
+        //int b = getInt();
+        double a = getDouble();
+        double b = getDouble();
+        
+     //  double a = 1.5555;
+     //  double b = 1.1111;
+        char mo = getOperation();
+        double c = calc(a, b, mo);
+        // int c = calc (a, b, mo);
+        System.out.println("Результат операции: " + c);
+    }
+
+    public static double getDouble()
+    {
+        System.out.println("Введите число:");
+        // int num;
+        double num;
+        //num = scan.nextInt();
+        num = scan.nextDouble();
+        return num;
+
+    }
+
+    public static char getOperation()
+    {
+        System.out.println("Введите операцию:");
+        char mo;
+        mo = scan.next().charAt(0);
+        return mo;
+    }
+/*
+    public static int calc(int num1, int num2, char mo)
+    {
+        int result;
+        switch (mo)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            default:
+                System.out.println("Введите тип операции: + сложение, - вычетание, / деление, * умножение");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+     }
+     */
+
+    public static double calc(double num1, double num2, char mo)
+    {
+        double result;
+        switch (mo)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            default:
+                System.out.println("Введите тип операции: + сложение, - вычетание, / деление, * умножение");
+                result = calc(num1, num2, getOperation());
+              /*  String.format("%.2f", result);*/
+        }
+        return result;
+    }
+}
