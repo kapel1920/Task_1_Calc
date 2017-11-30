2310ec7825450e3e4bff3b7ecc7bd0a850a31213
Andrey Veselkov <kapel1920@rambler.ru>
2017-11-30 18:24:38 +0300

    Task_1 final commit

--- 2/src/com/company/Main.java
+++ 2/src/com/company/Main.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 
 public class Main
 {
-    //Округление до 4-х знаков после запятой
-    private static DecimalFormat df4 = new DecimalFormat(".####");
+    //The main function
+    //Round up to 4 decimal places
+    /**
+     * @author Veselkov Andrey
+     *
+     */
 
-    //Ввод данных
+    private static DecimalFormat df4 = new DecimalFormat(".####");
     static Scanner scan = new Scanner(System.in);
 
-    //Основная функция
     public static void main(String[] args)
     {
         double a = getDouble();
@@ -23,7 +26,12 @@ public class Main
         System.out.println("Результат операции: " + df4.format(result));
     }
 
-    //Ввод чисел
+    //The method reads the value from the console and returns it
+
+    /**
+     * @author Veselkov Andrey
+     * @return num Value for Calculation
+     */
     public static double getDouble()
     {
         System.out.println("Введите число:");
@@ -32,7 +40,12 @@ public class Main
         return num;
     }
 
-    //Ввод операции
+    //The method reads the value from the console and returns a character with the operation (+, -, * or /)
+
+    /**
+     * @author Veselkov Andrey
+     * @return mo Type of Operation
+     */
     public static char getOperation()
     {
         System.out.println("Введите операцию (+, -, /, *) ");
@@ -41,7 +54,18 @@ public class Main
         return mo;
     }
 
-    //Расчет
+    //Calculation
+    //The method executes a mathematical operation on numbers (num1) and (num2), which is defined by mo.
+    /**
+     * Validates a chess move.
+     *
+     * @author Veselkov Andrey
+     * @param num1 Location of piece being moved
+     * @param num2 Rank of piece being moved
+     * @param mo Type of mathematical operation (addition +, subtraction -, multiplication *, division /).
+     * @return result of mathematical operation
+    invalid */
+
     public static double calc(double num1, double num2, char mo)
     {
         double result;
